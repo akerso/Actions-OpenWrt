abc38509f68538a127b6bee24eb6cd6d3157132e
akerso <70606558+akerso@users.noreply.github.com>
2021-06-26 00:59:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git linenol https://github.com/garypang13/openwrt-packages' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git openwrt-bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git luci-theme-edge https://github.com/garypang13/luci-theme-edge' feeds.conf.default
+
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 rm -rf ./package/lean/argon&& git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置
