e34839e8284b06fc517929da26aed04a8eff8604
akerso <70606558+akerso@users.noreply.github.com>
2021-07-04 07:54:38 +0800

    Update diy-part2.sh

--- ac2100/diy-part2.sh
+++ ac2100/diy-part2.sh
@@ -13,4 +13,4 @@ sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Ma
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
-sed -i '1i src/gz openwrt_custom https://op.supes.top/packages/mipsel' /etc/opkg/distfeeds.conf
+#sed -i '1i src/gz openwrt_custom https://op.supes.top/packages/mipsel' /etc/opkg/distfeeds.conf
