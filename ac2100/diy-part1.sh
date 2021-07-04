b362d119283bdf571256fcf5dffd64acd1161be9
akerso <70606558+akerso@users.noreply.github.com>
2021-07-04 18:09:55 +0800

    Update diy-part1.sh

--- ac2100/diy-part1.sh
+++ ac2100/diy-part1.sh
@@ -8,7 +8,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-#rm -rf ./package/lean/luci-theme-argon&& git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
-#git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置
+rm -rf ./package/lean/luci-theme-argon&& git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置
 #git clone https://github.com/garypang13/luci-theme-edge package/lean/luci-theme-edge
 
